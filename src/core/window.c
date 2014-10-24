1979dc92d8871c852d1d9bc07832de6b7f39f99b
infirit <infirit@gmail.com>
2014-10-24 13:38:54 +0200

    don't show shadow for maximized windows
    based on metacity commit: 72d1c6a6b9a85391444bb5ff8c5de1e3aabd3fd9
    From: Alberts Muktupāvels <alberts.muktupavels@gmail.com>

--- src/core/window.c
+++ src/core/window.c
@@ -2637,6 +2637,8 @@ meta_window_maximize (MetaWindow        *window,
       /* move_resize with new maximization constraints
        */
       meta_window_queue(window, META_QUEUE_MOVE_RESIZE);
+
+      meta_compositor_maximize_window (window->display->compositor, window);
     }
 }
 
@@ -2751,6 +2753,8 @@ meta_window_unmaximize (MetaWindow        *window,
 
       recalc_window_features (window);
       set_net_wm_state (window);
+
+      meta_compositor_unmaximize_window (window->display->compositor, window);
     }
 }
 
@@ -8271,6 +8275,12 @@ Window
 meta_window_get_xwindow (MetaWindow *window)
 {
   return window->xwindow;
+}
+
+gboolean
+meta_window_is_maximized (MetaWindow *window)
+{
+  return META_WINDOW_MAXIMIZED (window);
 }
 
 /**
