db3591485dfa1ce55b765713b6e90e9081197fef
infirit <infirit@gmail.com>
2014-10-24 12:51:12 +0200

    add support for _GTK_FRAME_EXTENTS
    Support for _GTK_FRAME_EXTENTS are based on mutter.
    
    Based on metacity commit: e132e2a700c4b50c93eae064d8fd1769b67baf06
    By: Alberts Muktupāvels <alberts.muktupavels@gmail.com>

--- src/core/atomnames.h
+++ src/core/atomnames.h
@@ -62,6 +62,7 @@ item(_GNOME_PANEL_ACTION_MAIN_MENU)
 item(_GNOME_PANEL_ACTION_RUN_DIALOG)
 item(_METACITY_SENTINEL)
 item(_METACITY_VERSION)
+item(_GTK_FRAME_EXTENTS)
 item(WM_CLIENT_MACHINE)
 item(MANAGER)
 item(TARGETS)
@@ -69,7 +70,6 @@ item(MULTIPLE)
 item(TIMESTAMP)
 item(VERSION)
 item(ATOM_PAIR)
-item(_GTK_FRAME_EXTENTS)
 
 /* Oddities: These are used, and we need atoms for them,
  * but when we need all _NET_WM hints (i.e. when we're making
